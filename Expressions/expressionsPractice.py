27940baa56c27b6620ba29f4bb768cc11ced2671
Maddie-Wharff <maddie.wharff@ucas-edu.net>
2024-09-11 19:41:02 +0000

    Nearly finished the expressions on python

--- Expressions/expressionsPractice.py
+++ Expressions/expressionsPractice.py
@@ -1,9 +1,26 @@
 print(7-24/8*4+6)
 print(18/3-7+2*5)
 print(6*4/12+72/8-9)
-num = 17-6/2
-print(num+4*3)
+frstnum = 17-6/2
+print(frstnum+4*3)
 secNum = -2*1
 thrdNum = -2*4
 frthNum = -2*2
-secNum*thrdNum-frthNum/frthNum
+fifNum = secNum*thrdNum-frthNum/frthNum
+sixNum = 6+2-3
+print(fifNum+sixNum)
+sevNum = 3-4*7
+egtNum = sevNum/5
+ninNum = -1*egtNum
+print(ninNum-2*24/6)
+tenNum = 3*5**2/15
+elevNum = 5-2**2
+print(tenNum-elevNum)
+twlvNum = 1**4*2**2+3**3
+thrtNum = twlvNum-2**5
+print(thrtNum/4)
+fortenNum = 22/2-2*5
+fiftenNum = fortenNum**2
+sixtenNum = 4-6/6
+sevtenNum = sixtenNum**2
+print(fiftenNum+sevtenNum)
