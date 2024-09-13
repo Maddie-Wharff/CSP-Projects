bad4b5b56d9cb8ba62f2645d148315e1fa50f209
Maddie-Wharff <maddie.wharff@ucas-edu.net>
2024-09-13 19:40:06 +0000

    Wrote more expressions on C

--- Expressions/expressionsPractice.c
+++ Expressions/expressionsPractice.c
@@ -2,7 +2,8 @@
 #include <math.h>
 
 int main(void){
-    printf("%d", 7-24/8*4+6);
-    printf("%d", 18/3-7+2*5);
+    printf("%d \n", 7-24/8*4+6);
+    printf("%d \n", 18/3-7+2*5);
+    printf("%d \n", 6*4/12+72/8-9);
     return 0;
 }
