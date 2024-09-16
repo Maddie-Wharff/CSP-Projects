39f787804ccb48efc05c277a47d592305900edfd
Maddie-Wharff <maddie.wharff@ucas-edu.net>
2024-09-16 18:45:05 +0000

    Continued Expressions c

--- Expressions/expressionsPractice.c
+++ Expressions/expressionsPractice.c
@@ -2,8 +2,15 @@
 #include <math.h>
 
 int main(void){
+    int numOne, numTwo, numTre, numFor, numFiv;
     printf("%d \n", 7-24/8*4+6);
     printf("%d \n", 18/3-7+2*5);
     printf("%d \n", 6*4/12+72/8-9);
+    numOne = 17-6/2;
+    printf("%d \n", numOne+4*3);
+    numTwo = -2*1;
+    numTre = -2*4;
+    numFor = -2*2;
+    numFiv = numTwo*numTre-numFor/numFor;
     return 0;
 }
