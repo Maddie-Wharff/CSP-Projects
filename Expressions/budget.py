ee71bc91ede76462cc20ae9e484c1c3e516c9ddc
Maddie-Wharff <maddie.wharff@ucas-edu.net>
2024-09-16 20:30:02 +0000

    Finished FCPY

--- Expressions/budget.py
+++ Expressions/budget.py
@@ -1,15 +1,23 @@
-print("This will calculate your budget for the month.")
-income = input("How much do you make in a month?: ")
-rent = input("How much is rent?: ")
-utilities = input("How much are utilities?: ")
-groceries = input("How much are groceries?: ")
-transportation = input("How much is transportation?: ")
-expenses = rent+utilities+groceries+transportation
-savings = income*.2
-total = income-expenses-savings
-prent = rent/income*100
-putilities = utilities/income*100
-pgroceries = groceries/income*100
-ptransportation = transportation/income*100
-pexpenses = expenses/income*100
-print("Your total expenses are:" expenses)
+print("This is going to calculate your budget for this month.")
+income = float(input("How much do you make every month?: "))
+print(f"Your income is: ${income:.2f}")
+rent = float(input("How much is rent?: "))
+utilities = float(input("How much are utilities?: "))
+groceries = float(input("How much are groceries?: "))
+transportation = float(input("How much is transportation?: "))
+expenses = rent + utilities + groceries + transportation
+savings = income * 0.2
+total = income - expenses - savings
+prent = rent / income
+putilities = utilities / income
+pgroceries = groceries / income
+ptransportation = transportation / income
+pexpenses = expenses / income
+print(f"Your expenses are: ${expenses:.2f}")
+print(f"Your savings are: ${savings:.2f}")
+print(f"Your total left to spend is: ${total:.2f}")
+print(f"Your rent is {prent:.2%} of your income")
+print(f"Your utilities are {putilities:.2%} of your income")
+print(f"Your groceries are {pgroceries:.2%} of your income")
+print(f"Your transportation is {ptransportation:.2%} of your income")
+print(f"Your expenses are {pexpenses:.2%} of your income")
